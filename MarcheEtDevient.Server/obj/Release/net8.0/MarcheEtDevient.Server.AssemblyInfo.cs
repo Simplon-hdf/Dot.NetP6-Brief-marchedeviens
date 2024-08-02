18d2eefca793248565bf9b9e3a8149aed6ab5433
Renaud Baussart <renaud.baussart@proton.me>
2024-08-02 13:37:59 +0200

    feat: add a IRepository in the Repository folder

--- MarcheEtDevient.Server/obj/Release/net8.0/MarcheEtDevient.Server.AssemblyInfo.cs
+++ MarcheEtDevient.Server/obj/Release/net8.0/MarcheEtDevient.Server.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarcheEtDevient.Server")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e68438899186ea06290a2504e72506079e58815")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarcheEtDevient.Server")]
