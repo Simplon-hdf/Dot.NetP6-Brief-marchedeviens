eaa99a298bfa7c22e1a9f6e8aa0c6936309484ac
Renaud Baussart <renaud.baussart@proton.me>
2024-08-03 10:09:02 +0200

    feat: polishing the repository for publicationActu

--- MarcheEtDevient.Server/Repository/IRepository.cs
+++ MarcheEtDevient.Server/Repository/IRepository.cs
@@ -6,9 +6,9 @@
         {
             Task<T> GetById(TKey id);                   //Recupere par un id
             Task<IEnumerable<T>> GetAll();              //Recupere tout
-            void Add(T model);                    //creer
-            void Update(T model, TKey id);        // met a jour
-            void Delete(TKey id);                 // supprime
+            Task<bool> Add(T model);                    //creer avec un retour bool pour verifier l'action
+            Task<bool> Update(T model, TKey id);        // met a jour avec un retour bool pour verifier l'action
+            Task<bool> Delete(TKey id);                 // supprime avec un retour bool pour verifier l'action
         }
     }
 }
--- MarcheEtDevient.Server/Repository/PublicationActusRepository.cs
+++ MarcheEtDevient.Server/Repository/PublicationActusRepository.cs
@@ -8,17 +8,23 @@ public class PublicationActusRepository : IRepository<PublicationActu, string>
 {
     private readonly ApiDBContext _contexteDeBDD;   // intialisation d'une variable de type apiDBContext
     public PublicationActusRepository(ApiDBContext context) =>  _contexteDeBDD = context;   // ajout du contexte de program.cs a l'initialisation de ce repository
-    public async void Add(PublicationActu model)
+    public async Task<bool> Add(PublicationActu model)
     {
-        _contexteDeBDD.PublicationActus.Add(model);     // ajout une nouvell entrée dans la BDD a partir de celle fournie dans le EndPoint(point de connection de l'api)
-        await _contexteDeBDD.SaveChangesAsync();        // Sauvegarde des changement dans la BDD
+        _contexteDeBDD.PublicationActus.Add(model);                                         // ajout une nouvell entrée dans la BDD a partir de celle fournie dans le EndPoint(point de connection de l'api)
+        await _contexteDeBDD.SaveChangesAsync();                                            // Sauvegarde des changement dans la BDD
+        string id = model.IdPublication;                                                    // stock l'id du model dans une variable 
+        if (await _contexteDeBDD.PublicationActus.FindAsync(id) == null){return false; }    // verfication de la creation
+        return true;                                                                        // verification ok
     }
 
-    public async void Delete(string id)
+    public async Task<bool> Delete(string id)
     {
         var bddPublicationActuSupprimer = await _contexteDeBDD.PublicationActus.FindAsync(id);      // recherche de l'id qui est en parrametre dans la BDD et le stock dans une variable
+        if ( bddPublicationActuSupprimer == null){return false; }                                   // verfication de l'existance de cette id dans la table
         _contexteDeBDD.PublicationActus.Remove(bddPublicationActuSupprimer);                        // Suprime l'entree correspondante
         await _contexteDeBDD.SaveChangesAsync();                                                    // Sauvegarde des changement dans la BDD
+        if (await _contexteDeBDD.PublicationActus.FindAsync(id) != null) { return false; }          // verfication de la supression
+        return true;                                                                                // verification ok
     }
 
     public async Task<IEnumerable<PublicationActu>> GetAll()
@@ -34,12 +40,17 @@ public class PublicationActusRepository : IRepository<PublicationActu, string>
         return publicationActu;                                                                     // retourne publicationActu
     }
 
-    public async void Update(PublicationActu model, string id)
+    public async Task<bool> Update(PublicationActu model, string id)
     {
-        var dbPublicationActu = await _contexteDeBDD.PublicationActus.FindAsync(id);    // recherche de l'id qui est en parrametre dans la BDD et le stock dans une variable
-        dbPublicationActu.DatePublication = model.DatePublication;                      // remplace la date de publication dans la bdd par celle du model
-        dbPublicationActu.IdPublicationActu = model.IdPublication;                      // remplace l'id de publication dans la bdd par celle du model
-        dbPublicationActu.IdVideo = model.IdVideo;                                      // remplace l'id de la video dans la bdd par celle du model
-        await _contexteDeBDD.SaveChangesAsync();                                        // Sauvegarde des changement dans la BDD
+        var dbPublicationActu = await _contexteDeBDD.PublicationActus.FindAsync(id);  // recherche de l'id qui est en parrametre dans la BDD et le stock dans une variable
+        dbPublicationActu.DatePublication = model.DatePublication;                    // remplace la date de publication dans la bdd par celle du model
+        dbPublicationActu.IdPublicationActu = model.IdPublication;                    // remplace l'id de publication dans la bdd par celle du model
+        dbPublicationActu.IdVideo = model.IdVideo;                                    // remplace l'id de la video dans la bdd par celle du model
+        await _contexteDeBDD.SaveChangesAsync();                                      // Sauvegarde des changement dans la BDD
+        var dbVerifAction = await _contexteDeBDD.PublicationActus.FindAsync(id);      // recherche de l'id qui est en parrametre dans la BDD et le stock dans une variable
+        if (dbVerifAction.IdVideo != model.IdVideo || 
+            dbVerifAction.IdPublication != model.IdPublication ) {return false; }     // verfication de la modification
+        return true;                                                                  // verification ok
+
     }
 }
