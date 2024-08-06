dcc288942bfb5e2ef439aa6899a5548f9693925d
SamiraCemoi <samiracommeca@gmail.com>
2024-08-06 14:15:20 +0200

    feat: create repo galery

--- MarcheEtDevient.Server/obj/Debug/net8.0/MarcheEtDevient.Server.AssemblyInfo.cs
+++ MarcheEtDevient.Server/obj/Debug/net8.0/MarcheEtDevient.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarcheEtDevient.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9e7f489eebf046e3500e76aba64e40db66898fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0976e1afcd44892a1f3707020127ef8bdf13c1a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarcheEtDevient.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarcheEtDevient.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
