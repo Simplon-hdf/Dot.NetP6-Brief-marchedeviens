3477454ce2bced1a4a48f2bf64520c1724b7e853
Renaud Baussart <renaud.baussart@proton.me>
2024-08-06 12:30:00 +0200

    feat: create controlleur for utilisateur

--- MarcheEtDevient.Server/obj/Debug/net8.0/MarcheEtDevient.Server.AssemblyInfo.cs
+++ MarcheEtDevient.Server/obj/Debug/net8.0/MarcheEtDevient.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarcheEtDevient.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66c417cc2622fb9e1387281ce6e0e8987c183a1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02505f59fa44fedeb8862481f043926daa02ead8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarcheEtDevient.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarcheEtDevient.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
