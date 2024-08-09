834009bed98bade83095a177b6f8d2e6b12f6858
CestPasFo <c.jaworski12@hotmail.fr>
2024-08-09 10:52:22 +0200

    feat : ajout d'un DTO pour la connexion

---
+++ MarcheEtDevient.Server/ModelsDTO/LoginDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MarcheEtDevient.Server.ModelsDTO
+{
+    public class LoginDTO
+    {
+        public class UserDto
+        {
+            public string mailUtilisateur { get; set; } = string.Empty;
+            public string motDePasse { get; set; } = string.Empty;
+        }
+    }
+}
