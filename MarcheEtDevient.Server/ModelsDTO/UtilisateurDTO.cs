03053d2873abacf9f553b007f4880498e62a9e72
CestPasFo <c.jaworski12@hotmail.fr>
2024-08-09 10:57:11 +0200

    feat : ajout d'un DTO Utilisateur

---
+++ MarcheEtDevient.Server/ModelsDTO/UtilisateurDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MarcheEtDevient.Server.ModelsDTO
+{
+    public class UtilisateurDTO
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Mail { get; set; }
+
+        [MaxLength(50)]
+        public string? Prenom { get; set; }
+
+        [MaxLength(50)]
+        public string? Nom { get; set; }
+
+        public int? DistanceParcourue { get; set; }
+    }
+}
