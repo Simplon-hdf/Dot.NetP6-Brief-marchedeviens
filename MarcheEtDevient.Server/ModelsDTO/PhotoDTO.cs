efba24dc6a542cafd713fd288205dd820b027510
CestPasFo <c.jaworski12@hotmail.fr>
2024-08-09 11:09:36 +0200

    feat : ajout d'un DTO Photo

---
+++ MarcheEtDevient.Server/ModelsDTO/PhotoDTO.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MarcheEtDevient.Server.ModelsDTO
+{
+    public class PhotoDTO
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public DateOnly? Date {  get; set; }
+
+        [Required]
+        public byte[]? Donnee { get; set; }
+    }
+}
