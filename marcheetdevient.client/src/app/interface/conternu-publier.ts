05289c3d5bdeb46cdcb5014a953483cac3dc92f0
Renaud Baussart <132342520+RenaudBaussart@users.noreply.github.com>
2024-08-12 15:17:40 +0200

    feat: add interface for api endpoint

---
+++ marcheetdevient.client/src/app/interface/conternu-publier.ts
@@ -0,0 +1,7 @@
+export interface ConternuPublier {
+    id: number;
+    date: Date;
+    theme: string;
+    commentaire: string;
+    idPhoto: number|null;
+}
---
+++ marcheetdevient.client/src/app/interface/utilisateur.ts
@@ -0,0 +1,9 @@
+export interface Utilisateur {
+    id: number;
+    dateCreation: Date;
+    mail:string;
+    nom:string;
+    prenom:string;
+    telephone:string;
+    age: number;
+}
