200e881fe2b377dd8449469d70063c59aaefa985
Arthur <Utilisateur@HDFPC5CG7072VSJ>
2024-08-19 10:24:17 +0200

    fix: merge conflict in marcheetdevient.client

--- marcheetdevient.client/src/app/app.component.spec.ts
+++ marcheetdevient.client/src/app/app.component.spec.ts
@@ -1,9 +1,10 @@
 
 import { HttpTestingController } from '@angular/common/http/testing';
-import { ComponentFixture} from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { ComponentFixture } from '@angular/core/testing';
+
 import { TestBed } from '@angular/core/testing';
 
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -12,6 +13,7 @@ describe('AppComponent', () => {
       imports: []
     }).compileComponents();
   });
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
