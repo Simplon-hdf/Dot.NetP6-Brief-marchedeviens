7eeccaefc7043f4e257e632f301d2fc609a6aa3c
CestPasFo <c.jaworski12@hotmail.fr>
2024-08-19 14:05:33 +0200

    Fix: Mise en page des headers/footers communs aux pages

--- marcheetdevient.client/src/app/app.component.ts
+++ marcheetdevient.client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import {NavigationEnd, Router} from '@angular/router';
 
 
 interface WeatherForecast {
@@ -15,8 +15,18 @@ interface WeatherForecast {
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent{
+
+export class AppComponent implements OnInit{
   constructor(private router:Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+        if (!(event instanceof NavigationEnd)) {
+            return;
+        }
+        window.scrollTo(0, 0)
+    });
+  }
   allerPage(nomPage:string):void{
     this.router.navigate([`${nomPage}`]);
   }
