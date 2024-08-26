056a813b495f325fa451709206eb095b69c23406
Renaud Baussart <renaud.baussart@proton.me>
2024-08-26 09:08:06 +0200

    fix: authentification controlleur

--- marcheetdevient.client/src/app/service/api_handler/api-handler-connection.service.ts
+++ marcheetdevient.client/src/app/service/api_handler/api-handler-connection.service.ts
@@ -13,7 +13,7 @@ async connectionRequete(utilisateur : Login){
   this.httpClient.post(`https://localhost:7260/api/login?mailUtilisateur=${utilisateur.email}&motDePasse=${utilisateur.motDePasse}`, {
   }).subscribe((res: any) => {
     if (res.result) {
-      alert("Photo ajouter a l'appli")
+      alert("connection Reussi")
     } else {
       alert(res.mess)
     }
@@ -22,7 +22,7 @@ async connectionRequete(utilisateur : Login){
 
 async inscriptionRequete(utilisateur : Register){
   console.log(`le mail de l'utilisateur = ${utilisateur.email}`);
-  this.httpClient.post(`https://localhost:7260/register?Mail=${utilisateur.email}&Prenom=${utilisateur.prenom}&Nom=${utilisateur.nom}&Mdp=${utilisateur.motDePasse}&Age=${utilisateur.age}&Telephone=${utilisateur.telephone}`, {
+  this.httpClient.post(`https://localhost:7260/register?MailUtilisateur=${utilisateur.email}&prenomUtilisateur=${utilisateur.prenom}&nomUtilisateur=${utilisateur.nom}&motDePasse=${utilisateur.motDePasse}&ageUtilisateur=${utilisateur.age}&TelephoneUtilisateur=${utilisateur.telephone}`, {
   }).subscribe((res: any) => {
     if (res.result) {
       alert("Photo ajouter a l'appli");
