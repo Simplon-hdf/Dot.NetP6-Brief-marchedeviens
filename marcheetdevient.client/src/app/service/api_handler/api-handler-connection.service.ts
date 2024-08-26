152d7005a0636396753d16ea228383acd6d1e9bb
Renaud Baussart <renaud.baussart@proton.me>
2024-08-26 09:34:14 +0200

    fix: inscription api post

--- marcheetdevient.client/src/app/service/api_handler/api-handler-connection.service.ts
+++ marcheetdevient.client/src/app/service/api_handler/api-handler-connection.service.ts
@@ -10,7 +10,9 @@ export class ApiHandlerConnectionService {
 httpClient = inject(HttpClient);
 
 async connectionRequete(utilisateur : Login){
-  this.httpClient.post(`https://localhost:7260/api/login?mailUtilisateur=${utilisateur.email}&motDePasse=${utilisateur.motDePasse}`, {
+  this.httpClient.post(`https://localhost:7260/api/AuthentificationControlleur/Connection`, {
+    "mailUtilisateur": utilisateur.email,
+    "motDePasse": utilisateur.motDePasse,
   }).subscribe((res: any) => {
     if (res.result) {
       alert("connection Reussi")
@@ -22,12 +24,19 @@ async connectionRequete(utilisateur : Login){
 
 async inscriptionRequete(utilisateur : Register){
   console.log(`le mail de l'utilisateur = ${utilisateur.email}`);
-  this.httpClient.post(`https://localhost:7260/register?MailUtilisateur=${utilisateur.email}&prenomUtilisateur=${utilisateur.prenom}&nomUtilisateur=${utilisateur.nom}&motDePasse=${utilisateur.motDePasse}&ageUtilisateur=${utilisateur.age}&TelephoneUtilisateur=${utilisateur.telephone}`, {
+  this.httpClient.post(`https://localhost:7260/api/AuthentificationControlleur/Inscription`, {
+    "mailUtilisateur": utilisateur.email,
+    "prenomUtilisateur": utilisateur.prenom,
+    "nomUtilisateur": utilisateur.nom,
+    "motDePasse": utilisateur.motDePasse,
+    "ageUtilisateur": utilisateur.age,
+    "nTelephoneUtilisateur": utilisateur.telephone,
   }).subscribe((res: any) => {
-    if (res.result) {
-      alert("Photo ajouter a l'appli");
+    if (res.message) {
+      console.log('Réponse reçue:', Response)
+      alert("Inscription Reussi");
     } else {
-      alert(res.mess)
+      alert(res.error)
     }
   })
 }
